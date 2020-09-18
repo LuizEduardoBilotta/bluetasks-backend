f37995871581b55c619d2206d41120b434f674d7
Luiz Eduardo Bilotta <luizeduardobilotta@gmail.com>
2020-09-18 16:13:46 -0300

    AULA 20.3 - Criando as entidades e os repositórios

---
+++ src/main/java/br/com/bilotta/bluetasks/domain/task/Task.java
@@ -0,0 +1,89 @@
+package br.com.bilotta.bluetasks.domain.task;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import br.com.bilotta.bluetasks.domain.user.AppUser;
+
+@Entity
+public class Task {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	private String description;
+	
+	private LocalDate whenToDo;
+	
+	private boolean done = false;
+	
+	@ManyToOne
+	@JoinColumn(name = "app_user_id")
+	private AppUser appUser;
+	
+	
+	public Task() {
+		
+	}
+
+
+	public Task(String description, LocalDate whenToDo, boolean done) {
+		super();
+		this.description = description;
+		this.whenToDo = whenToDo;
+		this.done = done;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public LocalDate getWhenToDo() {
+		return whenToDo;
+	}
+
+
+	public void setWhenToDo(LocalDate whenToDo) {
+		this.whenToDo = whenToDo;
+	}
+
+
+	public boolean isDone() {
+		return done;
+	}
+
+
+	public void setDone(boolean done) {
+		this.done = done;
+	}
+
+
+	public AppUser getAppUser() {
+		return appUser;
+	}
+
+
+	public void setAppUser(AppUser appUser) {
+		this.appUser = appUser;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+	
+	
+}
---
+++ src/main/java/br/com/bilotta/bluetasks/domain/user/AppUserRepository.java
@@ -0,0 +1,11 @@
+package br.com.bilotta.bluetasks.domain.user;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppUserRepository extends CrudRepository<AppUser, Integer> {
+
+	AppUser findByUsername(String username);
+	
+}
