c5b410b3c80556b79ee527d1ad0a2a238c6f4155
Luiz Eduardo Bilotta <luizeduardobilotta@gmail.com>
2020-09-18 18:20:20 -0300

    AULA 20.7 - Removendo a exportação do repositório de usuários.

--- src/main/java/br/com/bilotta/bluetasks/domain/user/AppUserRepository.java
+++ src/main/java/br/com/bilotta/bluetasks/domain/user/AppUserRepository.java
@@ -1,9 +1,11 @@
 package br.com.bilotta.bluetasks.domain.user;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RepositoryRestResource(exported = false)//este recurso não permite a exposição da classe via webservice
 public interface AppUserRepository extends CrudRepository<AppUser, Integer> {
 
 	AppUser findByUsername(String username);
