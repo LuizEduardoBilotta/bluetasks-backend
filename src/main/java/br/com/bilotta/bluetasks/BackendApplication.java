1b4e7203b796be210b120626aa1a50eedc3c3158
Luiz Eduardo Bilotta <luizeduardobilotta@gmail.com>
2020-09-18 18:14:34 -0300

    AULA 20.6 - Expondo o id das tarefas via web services.

--- src/main/java/br/com/bilotta/bluetasks/BackendApplication.java
+++ src/main/java/br/com/bilotta/bluetasks/BackendApplication.java
@@ -2,12 +2,21 @@ package br.com.bilotta.bluetasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+import br.com.bilotta.bluetasks.domain.task.Task;
 
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication implements RepositoryRestConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+	
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		config.exposeIdsFor(Task.class);
 	}
 
 }
