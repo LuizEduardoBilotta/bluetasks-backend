16d9f294fc1a5428520c6be68abed05ea5765404
Luiz Eduardo Bilotta <luizeduardobilotta@gmail.com>
2020-09-18 20:08:34 -0300

    AULA 20.9 - Configurando o processo de validação para retornar os erros corretamente.

--- src/main/java/br/com/bilotta/bluetasks/BackendApplication.java
+++ src/main/java/br/com/bilotta/bluetasks/BackendApplication.java
@@ -2,8 +2,12 @@ package br.com.bilotta.bluetasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import br.com.bilotta.bluetasks.domain.task.Task;
 
@@ -17,6 +21,20 @@ public class BackendApplication implements RepositoryRestConfigurer {
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
 		config.exposeIdsFor(Task.class);
+	}
+	
+	
+	//Usando Validator para tirar o Strack Tracer de erro e deixar as mensagens das classes
+	@Bean
+	public Validator validator() {
+		return new LocalValidatorFactoryBean();
+	}
+	
+	@Override
+	public void configureValidatingRepositoryEventListener(ValidatingRepositoryEventListener vrel) {
+		Validator validator = validator();
+		vrel.addValidator("beforeCreate", validator);
+		vrel.addValidator("beforeSave", validator);
 	}
 
 }
