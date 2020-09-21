69815f80096ed6c9d157d9fa5487cc0bc4101646
Luiz Eduardo Bilotta <luizeduardobilotta@gmail.com>
2020-09-21 13:14:46 -0300

    AULA 20.22 - Escondendo informações da tarefa.

--- src/main/java/br/com/bilotta/bluetasks/domain/task/Task.java
+++ src/main/java/br/com/bilotta/bluetasks/domain/task/Task.java
@@ -14,6 +14,8 @@ import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import br.com.bilotta.bluetasks.domain.user.AppUser;
 
 @Entity
@@ -36,6 +38,7 @@ public class Task {
 	
 	@ManyToOne
 	@JoinColumn(name = "app_user_id")
+	@JsonIgnore
 	private AppUser appUser;
 	
 	
