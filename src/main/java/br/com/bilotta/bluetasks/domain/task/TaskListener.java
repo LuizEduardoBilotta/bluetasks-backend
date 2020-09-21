f369e53534037688811ee69d322eb18dedde29ed
Luiz Eduardo Bilotta <luizeduardobilotta@gmail.com>
2020-09-21 13:12:06 -0300

    AULA 20.21 - Atribuindo um usuário a uma tarefa.

---
+++ src/main/java/br/com/bilotta/bluetasks/domain/task/TaskListener.java
@@ -0,0 +1,28 @@
+package br.com.bilotta.bluetasks.domain.task;
+
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.PrePersist;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import br.com.bilotta.bluetasks.domain.user.AppUser;
+import br.com.bilotta.bluetasks.domain.user.AppUserRepository;
+
+public class TaskListener {
+
+	private static AppUserRepository appUserRepository;
+	
+	@PrePersist
+	public void onPrePersistHandler(Task task) {
+		if (task.getAppUser() == null) {
+			String username = SecurityContextHolder.getContext().getAuthentication().getName();
+			AppUser appUser = appUserRepository.findByUsername(username);
+			
+			if (appUser == null) {
+				throw new EntityNotFoundException("O usuário " + username + " não foi encontrado!");
+			}
+			
+			task.setAppUser(appUser);
+		}
+	}
+}
